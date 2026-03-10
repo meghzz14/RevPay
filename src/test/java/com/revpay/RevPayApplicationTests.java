29522dff61a2430e3d37d6b035897aeccca974bb
Meghana Umesh <meghanaumesh40@gmail.com>
2026-03-10 16:07:31 +0530

    Modified test folder

---
+++ src/test/java/com/revpay/RevPayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.revpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class RevPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
